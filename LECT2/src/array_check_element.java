b26c22d254075e6c24ee71e341dfddb4440ed2bc
rishika26patel <rishika26patel@gmail.com>
2026-04-13 03:22:21 +0530

    search

--- LECT2/src/array_check_element.java
+++ LECT2/src/array_check_element.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class array_check_element {
+    static boolean search(int[]arr,int key,int i){
+        if(i==arr.length) return false;
+        if(arr[i]==key) return true;
+        return search(arr,key,i+1);
+
+    }
+    public static void main(String[]args){
+        int arr[]={1,2,3,4,5};
+        int key=2;
+        int i=0;
+        System.out.println(search(arr,key ,i));
+    }
 }
