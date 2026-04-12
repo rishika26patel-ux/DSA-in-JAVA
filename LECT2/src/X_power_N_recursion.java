f6f20aa307780f1b72c714ec67c113be04a86680
rishika26patel <rishika26patel@gmail.com>
2026-04-13 02:01:55 +0530

    code

--- LECT2/src/X_power_N_recursion.java
+++ LECT2/src/X_power_N_recursion.java
@@ -1,26 +1,40 @@
 import java.util.Scanner;
 
-public class X_power_N_recursion {
+//public class X_power_N_recursion {
+//
+//    static int power(int x, int n) {
+//        // Base case
+//        if (n == 0)
+//            return 1;
+//
+//        // Recursive case
+//        return x * power(x, n - 1);
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter base (x): ");
+//        int x = sc.nextInt();
+//
+//        System.out.print("Enter power (n): ");
+//        int n = sc.nextInt();
+//
+//        int result = power(x, n);
+//        System.out.println("Result: " + result);
+//    }
+//}
 
-    static int power(int x, int n) {
-        // Base case
-        if (n == 0)
-            return 1;
 
-        // Recursive case
-        return x * power(x, n - 1);
+
+public class X_power_N_recursion{
+    static int power(int x, int n){
+        if(n==0) return 1; //base case
+        return x * power(x ,n-1); //recursive function
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter base (x): ");
-        int x = sc.nextInt();
-
-        System.out.print("Enter power (n): ");
-        int n = sc.nextInt();
-
-        int result = power(x, n);
-        System.out.println("Result: " + result);
+    public static void main(String[]args){
+        int x=2;
+        int n=3;
+        System.out.println(power(x ,n));
     }
 }
