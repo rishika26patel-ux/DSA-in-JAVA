705339617670ea2baf22adbac9c8204b1808b69e
rishika26patel <rishika26patel@gmail.com>
2026-04-13 02:49:13 +0530

    reverse

--- LECT2/src/reverse_using_recursion.java
+++ LECT2/src/reverse_using_recursion.java
@@ -1,17 +1,32 @@
-public class reverse_using_recursion {
+//public class reverse_using_recursion {
+//
+//    static int Number(int n, int rev) {
+//        // Base case
+//        if (n == 0) {
+//            return rev;
+//        }
+//        // Recursive call
+//        return Number(n / 10, rev * 10 + n % 10);
+//    }
+//
+//    public static void main(String[] args) {
+//        int num = 123;
+//        int result = Number(num, 0);
+//        System.out.println("Reversed: " + result);
+//    }
+//}
+//
 
-    static int Number(int n, int rev) {
-        // Base case
-        if (n == 0) {
-            return rev;
-        }
-        // Recursive call
-        return Number(n / 10, rev * 10 + n % 10);
+public class reverse_using_recursion {
+    static int rev=0;
+    static int reverse(int x){
+        if(x==0)return rev;
+        rev =10 * x%10;
+        return reverse (x/10);
     }
 
-    public static void main(String[] args) {
-        int num = 123;
-        int result = Number(num, 0);
-        System.out.println("Reversed: " + result);
+    public static void main(String[]args){
+        int x=12345;
+        System.out.println(reverse(x));
     }
 }
