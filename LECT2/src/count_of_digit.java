a5c5224af816a748c6d4e7bf909df48dc173fc31
rishika26patel <rishika26patel@gmail.com>
2026-04-13 03:03:34 +0530

    count

--- LECT2/src/count_of_digit.java
+++ LECT2/src/count_of_digit.java
@@ -1,3 +1,31 @@
-public class count_of_digit {
+//public class count_of_digit {
+//
+//    static int count(int n) {
+//        // Base case
+//        if (n < 10) {
+//            return 1;
+//        }
+//
+//        // Recursive call
+//        return 1 + count(n / 10);
+//    }
+//
+//    public static void main(String[] args) {
+//        int num = 12345;
+//        System.out.println("digits: " + count(num));
+//    }
+//}
 
+
+
+public class count_of_digit{
+    static int count(int x){
+        if(x==0) return 0;
+        return 1+count(x/10);
+    }
+    public static void main(String[] args){
+        int x=12345;
+        System.out.println(count(x));
+    }
 }
+
