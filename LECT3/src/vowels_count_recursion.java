5f5f8e32198ad2d55ecc0159fd69e6824d5c58f4
rishika26patel <rishika26patel@gmail.com>
2026-04-15 02:01:50 +0530

    count

--- LECT3/src/vowels_count_recursion.java
+++ LECT3/src/vowels_count_recursion.java
@@ -1,3 +1,14 @@
 public class vowels_count_recursion {
+    static int vowel(String str,int i){
+        if(i==str.length()) return 0;
+        char ch=str.charAt(i);
+        int val=(ch== 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') ? 1:0;
+                return val + vowel(str,i+1);
+    }
 
+    public static void main(String[]args){
+        String str= "hello world";
+        int i=0;
+        System.out.println(vowel(str,i));
+    }
 }
